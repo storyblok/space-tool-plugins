861f5004190a7774b6be091b9b7c2b84654d9f81
Eunjae Lee <hey@eunjae.dev>
2023-11-08 16:09:26 +0100

    feat(story): add load more button (#11)
    ## What?
    
    This PR adds "Load more" button, and the logic related to that.
    
    This PR also improves `getAppSession`. More details in the review.
    
    ## Why?
    
    JIRA: EXT-2043
    
    <!--
      Explain the **motivation** for making this change.
      What existing problem does the pull request solve?
      Are there any linked issues?
    -->
    
    ## How to test? (optional)
    
    <!--
      Demonstrate the code is solid.
      Example: The exact commands you ran and their output,
      screenshots / videos if the pull request changes UI.
    -->

--- story-starter/starters/nuxt/pages/index.vue
+++ story-starter/starters/nuxt/pages/index.vue
@@ -1,11 +1,38 @@
 <script setup lang="ts">
-const { data } = await useFetch(`/api/stories`);
+import type { ISbStoryData } from 'storyblok-js-client';
+
+const pageLoaded = ref(1);
+const { data } = await useFetch(`/api/stories`, {
+	query: {
+		perPage: 10,
+		page: pageLoaded,
+	},
+});
+
+const totalStoryCount = computed(() => data?.value?.total);
+const allStories = ref<ISbStoryData[]>(
+	(data.value?.stories ?? []) as ISbStoryData[]
+);
+
+watch(data, (newData) => {
+	allStories.value = [
+		...allStories.value,
+		...((newData?.stories || []) as ISbStoryData[]),
+	];
+});
+
+const isLoadedAll = computed(
+	() => allStories.value.length >= (totalStoryCount.value ?? 0)
+);
+
+const loadMore = () => {
+	pageLoaded.value += 1;
+};
 </script>
 
 <template>
-	<div v-if="data">
-		<div v-for="(item, index) in data.stories" :key="index">
-			<pre>{{ item.name }} (/{{ item.slug }})</pre>
-		</div>
+	<div v-for="(item, index) in allStories" :key="index">
+		<pre>{{ item.name }} (/{{ item.slug }})</pre>
 	</div>
+	<button @click="loadMore" v-if="!isLoadedAll">load more</button>
 </template>
