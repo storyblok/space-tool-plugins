ebf0295a6489ca435e8e880cb11627c8ad12a57b
Eunjae Lee <hey@eunjae.dev>
2023-12-21 17:23:04 +0100

    feat(story): add search box

---
+++ story-starter/starters/nuxt/components/SearchBar.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import debounce from 'just-debounce-it';
+const query = ref<string>();
+
+const props = defineProps<{
+	setQuery: (query: string | undefined) => void;
+}>();
+
+watch(
+	query,
+	debounce((newQuery: string | undefined) => {
+		props.setQuery(newQuery);
+	}, 500)
+);
+</script>
+
+<template>
+	<div
+		class="flex items-center px-4 py-4 border border-gray-200 rounded-md hover:border-primary"
+	>
+		<LucideSearch class="text-gray-500 search-icon" :size="14" />
+		<input
+			type="text"
+			placeholder="Search"
+			class="flex-1 ml-2 text-gray-700 outline-none"
+			v-model="query"
+		/>
+	</div>
+</template>
+
+<style scoped>
+.search-icon {
+	margin-left: 0.125rem;
+}
+</style>
