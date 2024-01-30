dc5b7b8179a09324a8eb76b32cfe64025c348ea2
Eunjae Lee <hey@eunjae.dev>
2024-01-30 11:13:35 +0100

    address feedback

--- story-starter/starters/nuxt/server/utils/getAppSession.ts
+++ story-starter/starters/nuxt/server/utils/getAppSession.ts
@@ -10,49 +10,8 @@ type AppSessionQuery = {
 	userId: number;
 };
 
-const getAppSessionQuery = (event: H3Event): AppSessionQuery | undefined => {
-	const appSession = event.context.appSession;
-	const query = getQuery(event);
-
-	const candidates = [
-		// When a page is already authenticated on the server side,
-		// and it's rendering a page that includes `useFetch()`,
-		// Nuxt directly runs the serverless function on the server side
-		// (without an actual HTTP request).
-		//
-		// In that case `event.context.appSession` exists.
-		[appSession?.spaceId, appSession?.userId],
-
-		// if this is a page request (/?spaceId=xxx&userId=yyy)
-		[query.spaceId, query.userId],
-		[query.space_id, query.user_id],
-	];
-
-	const referer = getHeader(event, 'referer');
-	if (referer) {
-		const refParams = new URL(referer).searchParams;
-		candidates.push([
-			refParams.get('spaceId') as string,
-			refParams.get('userId') as string,
-		]);
-		candidates.push([
-			refParams.get('space_id') as string,
-			refParams.get('user_id') as string,
-		]);
-	}
-
-	for (const candidate of candidates) {
-		if (candidate[0] && candidate[1]) {
-			return {
-				spaceId: toNumber(candidate[0]),
-				userId: toNumber(candidate[1]),
-			};
-		}
-	}
-};
-
 export const getAppSession = async (event: H3Event) => {
-	const appSessionQuery = getAppSessionQuery(event);
+	const appSessionQuery = extractSpaceIdAndUserId(event);
 	if (!isAppSessionQuery(appSessionQuery)) {
 		return;
 	}
@@ -65,14 +24,62 @@ export const getAppSession = async (event: H3Event) => {
 	return await sessionStore.get(appSessionQuery);
 };
 
+function extractSpaceIdAndUserId(event: H3Event): AppSessionQuery | undefined {
+	const appSession = event.context.appSession;
+	const query = getQuery(event);
+
+	// When a page is already authenticated on the server side,
+	// and it's rendering a page that includes `useFetch()`,
+	// Nuxt directly runs the serverless function on the server side
+	// (without an actual HTTP request).
+	//
+	// In that case `event.context.appSession` exists.
+	if (appSession?.spaceId && appSession?.userId) {
+		return convertToAppSessionQuery(appSession?.spaceId, appSession?.userId);
+	}
+
+	// if this is a page request (/?spaceId=xxx&userId=yyy)
+	if (query.spaceId && query.userId) {
+		return convertToAppSessionQuery(query.spaceId, query.userId);
+	}
+	if (query.space_id && query.user_id) {
+		return convertToAppSessionQuery(query.space_id, query.user_id);
+	}
+
+	const referer = getHeader(event, 'referer');
+	if (referer) {
+		const refererParams = new URL(referer).searchParams;
+		if (refererParams.get('spaceId') && refererParams.get('userId')) {
+			return convertToAppSessionQuery(
+				refererParams.get('spaceId'),
+				refererParams.get('userId')
+			);
+		}
+
+		if (refererParams.get('space_id') && refererParams.get('user_id')) {
+			return convertToAppSessionQuery(
+				refererParams.get('space_id'),
+				refererParams.get('user_id')
+			);
+		}
+	}
+}
+
+function convertToAppSessionQuery(spaceId: any, userId: any): AppSessionQuery {
+	return {
+		spaceId: toNumber(spaceId),
+		userId: toNumber(userId),
+	};
+}
+
 const toNumber = (value: any) => {
 	if (typeof value === 'string') {
 		return parseInt(value, 10);
-	} else if (typeof value === 'number') {
-		return value;
-	} else {
-		throw new Error(
-			`Expected to be string or number. Actual value: ${JSON.stringify(value)}`
-		);
 	}
+	if (typeof value === 'number') {
+		return value;
+	}
+	throw new Error(
+		`Expected to be string or number. Actual value: ${JSON.stringify(value)}`
+	);
 };
