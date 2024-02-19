ceb6bf1e77fef2e7d46172e79b862c83dc76ac37
Bibiana Sebestianova <b.sebestianova@gmail.com>
2024-02-19 17:58:39 +0100

    feat(common): - added casing case

--- space-plugins/nuxt-base/middleware/persist-query.global.ts
+++ space-plugins/nuxt-base/middleware/persist-query.global.ts
@@ -16,18 +16,22 @@ export default defineNuxtRouteMiddleware((to, from) => {
 const isValidQuery = (query: unknown): query is Query =>
 	typeof query === 'object' &&
 	query !== null &&
-	'space_id' in query &&
-	typeof query.space_id === 'string' &&
-	'user_id' in query &&
-	typeof query.user_id === 'string' &&
-	'user_is_admin' in query &&
-	typeof query.user_is_admin === 'string' &&
-	'space_name' in query &&
-	typeof query.space_name === 'string' &&
-	'space_is_trial' in query &&
-	typeof query.space_is_trial === 'string' &&
-	'user_lang' in query &&
-	typeof query.user_lang === 'string';
+	(('space_id' in query &&
+		typeof query.space_id === 'string' &&
+		'user_id' in query &&
+		typeof query.user_id === 'string' &&
+		'user_is_admin' in query &&
+		typeof query.user_is_admin === 'string' &&
+		'space_name' in query &&
+		typeof query.space_name === 'string' &&
+		'space_is_trial' in query &&
+		typeof query.space_is_trial === 'string' &&
+		'user_lang' in query &&
+		typeof query.user_lang === 'string') ||
+		('spaceId' in query &&
+			typeof query.spaceId === 'string' &&
+			'userId' in query &&
+			typeof query.userId === 'string'));
 
 type Query = {
 	space_id: string;
