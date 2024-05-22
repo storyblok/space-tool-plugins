c1071f9462ea645cbd9b121736f8e32b9da61bd9
Eunjae Lee <hey@eunjae.dev>
2024-05-22 17:24:51 +0200

    test

--- space-plugins/nuxt-base/server/middleware/01.handle-401.global.ts
+++ space-plugins/nuxt-base/server/middleware/01.handle-401.global.ts
@@ -3,10 +3,14 @@ export default defineEventHandler(async (event) => {
 		event.path === '/401' &&
 		event.headers.get('Referer') === 'https://app.storyblok.com/'
 	) {
+		console.log('💡 handling /401 - (1)');
 		return await sendRedirect(
 			event,
 			'https://app.storyblok.com/oauth/app_redirect',
 			302,
 		);
 	}
+
+	if (event.path === '/401') {
+		console.log('💡 handling /401 - (2)', event.headers.get('Referer'));
 });
