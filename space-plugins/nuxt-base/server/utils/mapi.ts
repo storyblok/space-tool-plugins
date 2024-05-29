9afaab41fe2009ecc73258c586acbae180084f97
Eunjae Lee <hey@eunjae.dev>
2024-05-29 10:44:52 +0200

    fix(space-nuxt-base): include all regions of MAPI hosts

--- space-plugins/nuxt-base/server/utils/mapi.ts
+++ space-plugins/nuxt-base/server/utils/mapi.ts
@@ -1,7 +1,20 @@
-import { US_CODE, getRegion } from '@storyblok/region-helper';
+import {
+	US_CODE,
+	EU_CODE,
+	CA_CODE,
+	AP_CODE,
+	CN_CODE,
+	getRegion,
+} from '@storyblok/region-helper';
+
+const HOSTS = {
+	[EU_CODE]: 'https://mapi.storyblok.com',
+	[US_CODE]: 'https://api-us.storyblok.com',
+	[CA_CODE]: 'https://api-ca.storyblok.com',
+	[AP_CODE]: 'https://api-ap.storyblok.com',
+	[CN_CODE]: 'https://app.storyblokchina.cn',
+};
 
 export function getManagementApiHost(spaceId: number) {
-	return getRegion(spaceId) === US_CODE
-		? 'https://api-us.storyblok.com'
-		: 'https://mapi.storyblok.com';
+	return HOSTS[getRegion(spaceId) || EU_CODE];
 }
