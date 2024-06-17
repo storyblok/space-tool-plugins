7b9b92e60c5cb7a819f79fc136696eed60f3d45b
Eunjae Lee <hey@eunjae.dev>
2024-06-17 15:48:08 +0200

    clean up useAppBridge

--- space-plugins/nuxt-base/composables/useAppBridge.ts
+++ space-plugins/nuxt-base/composables/useAppBridge.ts
@@ -1,8 +1,33 @@
-export const useAppBridge = () => {
+// Is this app using App Bridge?
+const KEY_ENABLED = 'sb_app_bridge_enabled';
+// App Bridge token
+const KEY_TOKEN = 'sb_app_bridge_token';
+
+const useAppBridgeEnabled = () => {
+	const enabled = ref(false);
+
+	onMounted(() => {
+		if (
+			new URLSearchParams(location.search).get('use_app_bridge') === 'true' ||
+			window.sessionStorage.getItem(KEY_ENABLED) === 'true'
+		) {
+			enabled.value = true;
+		}
+	});
+
+	return enabled;
+};
+
+const useAppBridgeMessages = () => {
+	const status = ref<
+		'idle' | 'authenticating' | 'authenticated' | 'not-in-iframe'
+	>('idle');
+
 	const eventListener = async (event: MessageEvent) => {
 		if (event.data.action === 'validated') {
+			status.value = 'authenticated';
 			const token = event.data.token;
-			await $fetch('/api/_bridge_validate', {
+			await $fetch('/api/_app_bridge', {
 				method: 'POST',
 				body: JSON.stringify({ token }),
 			});
@@ -16,12 +41,21 @@ export const useAppBridge = () => {
 
 	onMounted(async () => {
 		window.addEventListener('message', eventListener);
+	});
 
+	const init = () => {
 		const isInIframe = window.top !== window.self;
 		if (!isInIframe) {
+			status.value = 'not-in-iframe';
 			return;
 		}
 
+		if (sessionStorage.getItem(KEY_TOKEN)) {
+			status.value = 'authenticated';
+			return;
+		}
+
+		status.value = 'authenticating';
 		const params = new URLSearchParams(location.search);
 		const protocol = params.get('protocol');
 		const host = params.get('host');
@@ -33,5 +67,27 @@ export const useAppBridge = () => {
 			},
 			`${protocol}//${host}`,
 		);
-	});
+	};
+
+	return {
+		status,
+		init,
+	};
+};
+
+export const useAppBridge = () => {
+	const enabled = useAppBridgeEnabled();
+	const { status, init } = useAppBridgeMessages();
+
+	watch(
+		enabled,
+		(value) => {
+			if (value) {
+				init();
+			}
+		},
+		{ immediate: true },
+	);
+
+	return { status };
 };
---
+++ space-plugins/nuxt-base/layouts/default.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+const { status } = useAppBridge();
+</script>
+
+<template>
+	<slot v-if="status === 'authenticated'" />
+</template>
