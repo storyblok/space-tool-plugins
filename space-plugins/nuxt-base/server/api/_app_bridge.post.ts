bd609117b8bdd49af9f17141edf37194bc09e9cb
Eunjae Lee <hey@eunjae.dev>
2024-06-24 14:39:54 +0200

    extract verifyAppBridgeToken

--- space-plugins/nuxt-base/server/api/_app_bridge.post.ts
+++ space-plugins/nuxt-base/server/api/_app_bridge.post.ts
@@ -1,22 +1,4 @@
-import jwt, { type VerifyCallback } from 'jsonwebtoken';
-import { DecodedToken, VerifyResponse } from '~/types/appBridge';
-
-export default defineEventHandler(async (event): Promise<VerifyResponse> => {
+export default defineEventHandler(async (event) => {
 	const { token } = await readBody(event);
-	try {
-		return { ok: true, result: await verifyToken(token, env('CLIENT_SECRET')) };
-	} catch (error) {
-		return { ok: false, error };
-	}
+	return verifyAppBridgeToken(token);
 });
-
-async function verifyToken(
-	token: string,
-	secret: string,
-): Promise<DecodedToken> {
-	return new Promise((resolve, reject) => {
-		const verifyCallback: VerifyCallback = (err, decoded) =>
-			err ? reject(err) : resolve(decoded as DecodedToken);
-		jwt.verify(token, secret, verifyCallback);
-	});
-}
