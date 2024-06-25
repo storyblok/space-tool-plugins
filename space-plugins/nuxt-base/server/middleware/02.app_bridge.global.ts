37156a861c0fe745be3349295277ceaccd7380f2
Eunjae Lee <hey@eunjae.dev>
2024-06-25 14:41:58 +0200

    skip app bridge middleware for some endpoints

--- space-plugins/nuxt-base/server/middleware/02.app_bridge.global.ts
+++ space-plugins/nuxt-base/server/middleware/02.app_bridge.global.ts
@@ -1,9 +1,14 @@
+const ALLOWED_ENDPOINTS = ['/api/_app_bridge', '/api/_oauth'];
+
 export default defineEventHandler(async (event) => {
 	const appConfig = useAppConfig();
 	if (!appConfig.appBridge.enabled) {
 		return;
 	}
 	if (!event.path.startsWith('/api/')) {
+		return;
+	}
+	if (ALLOWED_ENDPOINTS.includes(event.path)) {
 		return;
 	}
 	const token = getHeader(event, APP_BRIDGE_TOKEN_HEADER_KEY);
