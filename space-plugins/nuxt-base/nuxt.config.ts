037b52c248384a4db5030116ec68e9a4e1a7ab09
Eunjae Lee <hey@eunjae.dev>
2024-07-25 09:59:08 +0200

    feat(common): run tunnel on start up

--- space-plugins/nuxt-base/nuxt.config.ts
+++ space-plugins/nuxt-base/nuxt.config.ts
@@ -1,3 +1,6 @@
+import type { Plugin } from 'vite';
+import { execaCommand } from 'execa';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	devtools: { enabled: true },
@@ -6,4 +9,26 @@ export default defineNuxtConfig({
 			dirs: ['./utils', './server/utils'],
 		},
 	},
+	vite: {
+		plugins: [runTunnelPlugin()],
+	},
 });
+
+function runTunnelPlugin(): Plugin {
+	let isBuildingClientSideOfNuxt = false;
+	const tunnelCommand = process.env.TUNNEL_COMMAND;
+	return {
+		name: 'run-tunnel',
+		apply: 'serve',
+		config(config) {
+			isBuildingClientSideOfNuxt = config.build?.ssr !== true;
+		},
+		buildStart() {
+			if (tunnelCommand && isBuildingClientSideOfNuxt) {
+				// using execa@8.0.1 because we're using a bit lower version of Node.js than what execa@9 requires.
+				// https://github.com/sindresorhus/execa/blob/v8.0.1/readme.md
+				execaCommand(tunnelCommand);
+			}
+		},
+	};
+}
