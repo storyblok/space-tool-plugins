82d59d1e4d765284b68c280fc7eaa0ee2e7cc026
Demetrius Feijóo <demetrius.feijoo@storyblok.com>
2024-08-05 14:05:57 -0300

    feat: add modal confirm to the nuxt-base-ui layer

---
+++ space-plugins/nuxt-base-ui/components/ModalConfirm.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+defineProps<{
+	title?: string;
+	message?: string;
+}>();
+
+const emit = defineEmits(['confirmed']);
+
+const modalConfirmRef = ref<InstanceType<typeof BaseModal>>();
+
+const show = () => modalConfirmRef.value?.show();
+const close = () => modalConfirmRef.value?.close();
+
+defineExpose({ show, close });
+
+const onConfirmClicked = () => {
+	emit('confirmed');
+	close();
+};
+</script>
+
+<template>
+	<BaseModal ref="modalConfirmRef" :title="title" :message="message">
+		<template #actions>
+			<menu class="modal-action px-8 py-6 m-0 bg-gray-100">
+				<button
+					class="btn btn-tertiary"
+					title="Close the modal and cancel the action"
+				>
+					Cancel
+				</button>
+				<button
+					class="btn btn-primary"
+					title="Confirm the action"
+					@click.prevent="onConfirmClicked"
+				>
+					Confirm
+				</button>
+			</menu>
+		</template>
+	</BaseModal>
+</template>
